043b8735b67b5eb79889adec400acb34faae4402
Shift <shift@laravelshift.com>
2024-09-10 06:33:41 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,42 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo('/');
+
+        $middleware->validateCsrfTokens(except: [
+            'xmlfeed',
+        ]);
+
+        $middleware->web(\App\Http\Middleware\LocaleMiddleware::class);
+
+        $middleware->throttleApi();
+
+        $middleware->group('admin', [
+            'auth',
+            'access.routeNeedsPermission:view-backend',
+            'timeout',
+        ]);
+
+        $middleware->alias([
+            'OnlyUsers' => \App\Http\Middleware\OnlyUsers::class,
+            'access.routeNeedsPermission' => \App\Http\Middleware\RouteNeedsPermission::class,
+            'access.routeNeedsRole' => \App\Http\Middleware\RouteNeedsRole::class,
+            'checkDeletedUserOffer' => \App\Http\Middleware\CheckDeletedUserHasOffer::class,
+            'checkOfferValues' => \App\Http\Middleware\CheckSessionHasOfferValues::class,
+            'checkPropertyId' => \App\Http\Middleware\CheckSessionHasOfferPropertyId::class,
+            'checkSignatureValues' => \App\Http\Middleware\CheckSessionHasOfferSignature::class,
+            'timeout' => \App\Http\Middleware\SessionTimeout::class,
+        ]);
+
+        $middleware->priority([
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\Authenticate::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
